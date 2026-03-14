90da293b67217d45779a435684976c5e99bf9ab1
Vinay Solaskar <vinaysolaskar1@gmail.com>
2026-03-14 16:19:57 +0530

    Fix critical Trivy finding by restring outbound traffic

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,13 +25,12 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Outbound HTTPS
   egress {
-    description = "Allow outbound HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow internal outbound traffic only"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   tags = {
