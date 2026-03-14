cc9c71d1ddd0eec0e580a6394201d6da05a5664b
Vinay Solaskar <vinaysolaskar1@gmail.com>
2026-03-14 16:11:01 +0530

    Fix critical Trivy egress vulnerability

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,12 +25,21 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Outbound traffic
+  # Outbound HTTP
   egress {
-    description = "Allow outbound internet access"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description = "Allow outbound HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound HTTPS
+  egress {
+    description = "Allow outbound HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
